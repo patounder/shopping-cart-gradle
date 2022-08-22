63bb79dd22ba871ad53df684c15e755f1c870b10
patricio.quiroz <pmquirozvargas@gmail.com>
2022-08-22 17:47:15 -0400

    listener with dev profile

--- src/main/java/com/globant/shoppingcart/messaging/Publisher.java
+++ src/main/java/com/globant/shoppingcart/messaging/Publisher.java
@@ -3,8 +3,10 @@ package com.globant.shoppingcart.messaging;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Profile("prod")
 @Service
 public class Publisher {
 
